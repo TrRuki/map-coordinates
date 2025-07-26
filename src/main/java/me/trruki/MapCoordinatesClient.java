2f5e114da86b2ae52ca0f7fd5dc3c5de4b0e2471
Anvarys <105349498+Anvarys@users.noreply.github.com>
2025-07-26 23:47:50 +0300

    - added support for both hands - added click action to copy coordinates to clipboard

--- src/main/java/me/trruki/MapCoordinatesClient.java
+++ src/main/java/me/trruki/MapCoordinatesClient.java
@@ -9,9 +9,12 @@ import net.minecraft.component.DataComponentTypes;
 import net.minecraft.component.type.MapDecorationsComponent;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
-import net.minecraft.text.Text;
+import net.minecraft.text.*;
+import net.minecraft.util.Formatting;
 
 public class MapCoordinatesClient implements ClientModInitializer {
+
+	private double x;
 
 	@Override
 	public void onInitializeClient() {
@@ -21,13 +24,29 @@ public class MapCoordinatesClient implements ClientModInitializer {
 						MinecraftClient client = MinecraftClient.getInstance();
 						if (client.player != null){
 							ItemStack mainItem = client.player.getInventory().getSelectedStack();
-							if (mainItem == null || ! mainItem.isOf(Items.FILLED_MAP)){
+							ItemStack offItem = client.player.getOffHandStack();
+							if ((mainItem == null || ! mainItem.isOf(Items.FILLED_MAP)) && (offItem == null || ! offItem.isOf(Items.FILLED_MAP))){
 								client.inGameHud.getChatHud().addMessage(Text.literal("§f[§dTreasure Map Coordinates§f] §cYou are not holding a treasure map"));
-							} else if (mainItem.get(DataComponentTypes.MAP_DECORATIONS).decorations().values().toArray().length == 0) {
+							} else if ((mainItem.get(DataComponentTypes.MAP_DECORATIONS) == null || mainItem.get(DataComponentTypes.MAP_DECORATIONS).decorations().values().toArray().length == 0) && (offItem.get(DataComponentTypes.MAP_DECORATIONS) == null || offItem.get(DataComponentTypes.MAP_DECORATIONS).decorations().values().toArray().length == 0)) {
 								client.inGameHud.getChatHud().addMessage(Text.literal("§f[§dTreasure Map Coordinates§f] §cYou are holding a map, but it's not a treasure map"));
-							} else {
+							} else if (mainItem.get(DataComponentTypes.MAP_DECORATIONS) != null && mainItem.get(DataComponentTypes.MAP_DECORATIONS).decorations().values().toArray().length != 0 && offItem.get(DataComponentTypes.MAP_DECORATIONS) != null && offItem.get(DataComponentTypes.MAP_DECORATIONS).decorations().values().toArray().length != 0){
 								MapDecorationsComponent.Decoration decoration = (MapDecorationsComponent.Decoration) mainItem.get(DataComponentTypes.MAP_DECORATIONS).decorations().values().toArray()[0];
-								client.inGameHud.getChatHud().addMessage(Text.literal("§f[§dTreasure Map Coordinates§f] §aX: "+decoration.x()+" §8| §aZ: "+decoration.z()));
+								MapDecorationsComponent.Decoration decoration2 = (MapDecorationsComponent.Decoration) offItem.get(DataComponentTypes.MAP_DECORATIONS).decorations().values().toArray()[0];
+								int x = (int) decoration.x();
+								int z = (int) decoration.z();
+								int x2 = (int) decoration2.x();
+								int z2 = (int) decoration2.z();
+								client.inGameHud.getChatHud().addMessage(Text.literal("§f[§dTreasure Map Coordinates§f] §fMain hand: §aX: "+x+" §8| §aZ: "+z+" §8| §fOff hand: §aX: "+x2+" §8| §aZ: "+z2).setStyle(Style.EMPTY.withClickEvent(new ClickEvent.CopyToClipboard("Main hand: X: "+x+" Z: "+z+" | Off hand: X: "+x2+" Z: "+z2)).withHoverEvent(new HoverEvent.ShowText(Text.translatable("chat.copy.click").setStyle(Style.EMPTY.withColor(Formatting.GREEN))))));
+							} else if (mainItem.get(DataComponentTypes.MAP_DECORATIONS) != null && mainItem.get(DataComponentTypes.MAP_DECORATIONS).decorations().values().toArray().length != 0){
+								MapDecorationsComponent.Decoration decoration = (MapDecorationsComponent.Decoration) mainItem.get(DataComponentTypes.MAP_DECORATIONS).decorations().values().toArray()[0];
+								int x = (int) decoration.x();
+								int z = (int) decoration.z();
+								client.inGameHud.getChatHud().addMessage(Text.literal("§f[§dTreasure Map Coordinates§f] §aX: "+x+" §8| §aZ: "+z).setStyle(Style.EMPTY.withClickEvent(new ClickEvent.CopyToClipboard("X: "+x+" Z: "+z)).withHoverEvent(new HoverEvent.ShowText(Text.translatable("chat.copy.click").setStyle(Style.EMPTY.withColor(Formatting.GREEN))))));
+							} else if (offItem.get(DataComponentTypes.MAP_DECORATIONS) != null && offItem.get(DataComponentTypes.MAP_DECORATIONS).decorations().values().toArray().length != 0){
+								MapDecorationsComponent.Decoration decoration = (MapDecorationsComponent.Decoration) offItem.get(DataComponentTypes.MAP_DECORATIONS).decorations().values().toArray()[0];
+								int x = (int) decoration.x();
+								int z = (int) decoration.z();
+								client.inGameHud.getChatHud().addMessage(Text.literal("§f[§dTreasure Map Coordinates§f] §aX: "+x+" §8| §aZ: "+z).setStyle(Style.EMPTY.withClickEvent(new ClickEvent.CopyToClipboard("X: "+x+" Z: "+z)).withHoverEvent(new HoverEvent.ShowText(Text.translatable("chat.copy.click").setStyle(Style.EMPTY.withColor(Formatting.GREEN))))));
 							}
 						}
 						return 1;
